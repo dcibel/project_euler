764c0b03a0810ad5ecb5fb07890080f7215419c4
dcibiel <david.cibiel@widmee.com>
2012-07-17 13:45:19 +0200

    Fixed problem 10 solution

--- scala/euler10.scala
+++ scala/euler10.scala
@@ -2,14 +2,13 @@
 def isPrime(n:Long):Boolean = {
   if (n == 1) return true;
   
-  (2L to n).forall { x => n % x != 0 };
+  return (2L to math.sqrt(n).toLong).forall { x => n % x != 0L };
 }
 
 
 def sumPrimeNumbersUnder(max:Long):Long = {
-  val primeNumbers = (2L to max).filter { x => isPrime(x) }
-  return primeNumbers.reduceLeft(_+_);
+  val primeNumbersFrom3 = (3L to max by 2L).filter { x => isPrime(x) }
+  return 2L + primeNumbersFrom3.reduceLeft(_+_);
 }
-
 
 println(sumPrimeNumbersUnder(2000000L))
