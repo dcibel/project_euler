4c322594a0e1b283ffb68ca150b48dbd96f7c007
dcibiel <david.cibiel@widmee.com>
2012-10-09 15:15:34 +0200

    Solved problems 18 and 67

---
+++ scala/euler18.scala
@@ -0,0 +1,18 @@
+import scala.io.Source;
+
+
+def findBiggestPathScore(fileName:String):Long = {
+  val lines = Source.fromFile(fileName).getLines.foldLeft(Array[Array[Long]]()) {  (valueLines, line) =>
+    valueLines :+ line.split(" ").map { s => s.toLong };
+  }
+  var currentScores:IndexedSeq[Long] = lines(lines.length - 1);
+  for(i <- lines.length - 2 to 0 by -1) {
+    val line = lines(i);
+    currentScores = (0 until line.length).map { (j) =>
+      line(j) + scala.math.max(currentScores(j), currentScores(j + 1));
+    }
+  }
+  return currentScores(0);
+}
+
+println(findBiggestPathScore("euler18_input"));
