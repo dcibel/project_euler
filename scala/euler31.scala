f95d66497c20a656f4aa93e4965c9943f9d1ada0
dcibiel <david.cibiel@widmee.com>
2013-02-18 14:19:40 +0100

    Made problem 31 solution much faster

--- scala/euler31.scala
+++ scala/euler31.scala
@@ -1,29 +1,26 @@
 
 
-def findCombinationsOfCoinsToReachAmount(coins:List[Int], amount:Int):List[List[Int]] = {
+def countCombinationsOfCoinsToReachAmount(coins:List[Int], amount:Int):Int = {
   if (coins.length == 0) {
-    return List[List[Int]]();
+    return 0;
   }
 
 
   val coin = coins(0);
   if (coins.length == 1) {
-    return List(List(amount / coin));
+    return 1;
   }
 
   val remainingCoins = coins.slice(1, coins.length);
-  var combinations = List[List[Int]]();
+  var count = 0;
 
   (0 to amount / coin).foreach { i =>
-    findCombinationsOfCoinsToReachAmount(remainingCoins, amount - i * coin).foreach { subcombination =>
-      val combination = (i :: subcombination);
-      combinations = combinations :+ combination;
-    }
+    count = count + countCombinationsOfCoinsToReachAmount(remainingCoins, amount - i * coin);
   }
-  return combinations;
+  return count;
 }
 
 
 
 val coins = List(200, 100, 50, 20, 10, 5, 2, 1);
-println(findCombinationsOfCoinsToReachAmount(coins, 200).length);
+println(countCombinationsOfCoinsToReachAmount(coins, 200));
