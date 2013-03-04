daee4650871b459567c32b4ddac2b2ca0e96e184
dcibiel <david.cibiel@widmee.com>
2013-03-04 13:45:53 +0100

    Solved problem 47 in scala

---
+++ scala/euler46.scala
@@ -0,0 +1,37 @@
+
+def isPrime(n:Long):Boolean = {
+  if (n <= 1) return false;  
+  return (2L to math.sqrt(n).toLong).forall { x => n % x != 0L };
+}
+
+
+def completeSquareDoublesSequenceUntil(sequence:List[Long], limit:Long):List[Long] = {
+  var newSequence = List[Long]() ::: sequence;
+  while(newSequence.last < limit) {
+    val nextIndex = newSequence.length + 1L;
+    newSequence = newSequence :+ (2L * nextIndex * nextIndex);
+  }
+
+  return newSequence.slice(0, newSequence.length);
+}
+
+def findGoldbachConjectureCounterExample():Long = {
+  var number:Long = 9;
+  var squareDoubles = List(2L);
+  while(true) {
+    if (!isPrime(number)) {
+      squareDoubles = completeSquareDoublesSequenceUntil(squareDoubles, number);
+      var matchesConjecture = squareDoubles.exists { d =>
+        val remainder = number - d;
+        isPrime(remainder);
+      }
+      if (!matchesConjecture) {
+        return number;
+      }
+    }
+    number = number + 2;
+  }
+  return -1;
+}
+
+println(findGoldbachConjectureCounterExample());
